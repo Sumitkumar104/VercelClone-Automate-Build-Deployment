5cda72cd9e8eb9b51b91a5c8c1466f94f874a367
Sumitkumar104 <parjapatsumit104@gmail.com>
2024-06-19 18:13:06 +0530

    add some comments in s3-reverse proxy code

--- s3-reverse-proxy/index.js
+++ s3-reverse-proxy/index.js
@@ -1,30 +1,40 @@
-const express = require('express') // Importing the Express framework to create a server.
-const httpProxy = require('http-proxy') // Importing the http-proxy module for proxying HTTP requests.
+const express = require('express')
+const httpProxy = require('http-proxy')
 
-const app = express() // Creating an instance of the Express application.
-const PORT = 8080 // Setting the port number for the server to listen on.
+const app = express()
+// our reverse proxy server run on 8080 port 
+const PORT = 8080 
 
-const BASE_PATH = 'https://vercel-clone-outputs.s3.ap-south-1.amazonaws.com/__outputs' // Defining the base path for proxying requests.
 
-const proxy = httpProxy.createProxy() // Creating a proxy instance.
 
-app.use((req, res) => { // Adding middleware to handle incoming requests.
-    const hostname = req.hostname; // Extracting the hostname from the request.
-    const subdomain = hostname.split('.')[0]; // Extracting the subdomain from the hostname.
+// this Base path is obtain from the S3 bucket link-
+// This is our actual link obtain from S3 bucket where we store the build code of our github url https://vercel-clone-outputs.s3.ap-south-1.amazonaws.com/__outputs/a1/index.html
+const BASE_PATH = 'https://vercel-clone-outputs.s3.ap-south-1.amazonaws.com/__outputs'
+const proxy = httpProxy.createProxy()
+
+// localhost:8080/
+// Adding middleware to handle incoming requests.
+app.use((req, res) => {
+    const hostname = req.hostname;
+
+    // a1.localhost:8080
+    const subdomain = hostname.split('.')[0];  // a1
 
     // Custom Domain - DB Query
+    
+     // Generating the target URL based on the subdomain.
+    const resolvesTo = `${BASE_PATH}/${subdomain}`
 
-    const resolvesTo = `${BASE_PATH}/${subdomain}` // Generating the target URL based on the subdomain.
 
-    // Proxying the request to the resolved URL.
     return proxy.web(req, res, { target: resolvesTo, changeOrigin: true })
 })
 
-// Event listener for the 'proxyReq' event, allowing modification of the proxy request.
+
 proxy.on('proxyReq', (proxyReq, req, res) => {
-    const url = req.url; // Extracting the URL from the request.
-    if (url === '/') // Checking if the URL is the root path.
-        proxyReq.path += 'index.html' // Appending 'index.html' to the proxy request path.
+    const url = req.url;
+    if (url === '/')
+         // Appending 'index.html' to the proxy request path.
+        proxyReq.path += 'index.html'
 })
 
-app.listen(PORT, () => console.log(`Reverse Proxy Running at port..${PORT}`)) // Starting the server and logging a message upon successful start.
+app.listen(PORT, () => console.log(`Reverse Proxy Running at port..${PORT}`))
